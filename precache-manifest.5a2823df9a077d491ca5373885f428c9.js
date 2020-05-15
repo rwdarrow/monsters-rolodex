4da546fd665ca8a3313d7a061619595b02ff19f8
Robbie Darrow <rwdarrow4582@eagle.fgcu.edu>
2020-05-15 04:24:30 -0400

    Updates

---
+++ precache-manifest.5a2823df9a077d491ca5373885f428c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c079a67112a3cd71a042d431bbbca10a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "08e1284561960442a2bc",
+    "url": "/monsters-rolodex/static/css/main.f94fb859.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08e1284561960442a2bc",
+    "url": "/monsters-rolodex/static/js/main.37bc10da.chunk.js"
+  },
+  {
+    "revision": "e3f9453037e5f28f8167",
+    "url": "/monsters-rolodex/static/js/runtime-main.7c0f1f0f.js"
+  }
+]);
